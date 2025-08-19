90b2ae87271988383a43cd08274679be99387f09
adwitiyaglacs24-bit <adwitiya.gla_cs24@gla.ac.in>
2025-08-19 11:19:13 +0530

    Delete Java-Fundamental directory

--- Java-Fundamental/hello.java
+++
@@ -1,8 +0,0 @@
-public class hello {
-    public static void main (String[] args){
-        System.out.println("hiii");
-        System.out.println("hello");
-        System.out.println("hehr");
-    }
-    
-}
