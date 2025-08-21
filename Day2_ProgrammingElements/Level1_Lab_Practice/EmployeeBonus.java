c1b2f0959882d72c0bd9463a10bae688d30cd12f
adwitiyaglacs24-bit <adwitiya.gla_cs24@gla.ac.in>
2025-08-22 00:01:57 +0530

    seventeeth commit

---
+++ Day2_ProgrammingElements/Level1_Lab_Practice/EmployeeBonus.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class EmployeeBonus {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {   
+            System.out.print("Enter your salary: ");
+            double salary = sc.nextDouble();
+            System.out.print("Enter your years of service: ");
+            int years = sc.nextInt();
+            if (years > 5) {
+                double bonus = salary * 0.05;   
+                System.out.println("Your bonus is: " + bonus);
+            } else {
+                System.out.println("Sorry, no bonus (service must be > 5 years).");
+            }
+        }
+    }
+}
