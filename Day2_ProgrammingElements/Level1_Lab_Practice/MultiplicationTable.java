2b8fc0a6783b05b8477a3fc6a1556f7169aa2ee6
adwitiyaglacs24-bit <adwitiya.gla_cs24@gla.ac.in>
2025-08-22 00:03:24 +0530

    eighteenth commit

---
+++ Day2_ProgrammingElements/Level1_Lab_Practice/MultiplicationTable.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {   
+            System.out.print("Enter a number: ");
+            int number = sc.nextInt();
+            System.out.println("\nMultiplication Table of " + number + " from 6 to 9:");
+            for (int i = 6; i <= 9; i++) {
+                System.out.println(number + " * " + i + " = " + (number * i));
+            }
+        }
+    }
+}
