db0092dfb2e72d3672a6266c5b2a0fe8e20b65cf
adwitiyaglacs24-bit <adwitiya.gla_cs24@gla.ac.in>
2025-08-21 23:05:26 +0530

    first commit

--- Level1 Lab Practice/DivisibleBy5.java
+++ Level1 Lab Practice/DivisibleBy5.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class DivisibleBy5 {
     public static void main(String[] args) {
-        try (Scanner sc = new Scanner(System.in)) {
-            int number = sc.nextInt();
+        try (Scanner input = new Scanner(System.in)) {
+            int number = input.nextInt();
             if (number % 5 == 0) {
                 System.out.println("Is the number " + number + " divisible by 5? Yes");
             } else {
