12cf5a9d427eab271d069415cd6f0d32b398fc49
adwitiyaglacs24-bit <adwitiya.gla_cs24@gla.ac.in>
2025-08-21 23:37:16 +0530

    tenth commit

---
+++ Day2_ProgrammingElements/Level1_Lab_Practice/AlphabetCheck.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class AlphabetCheck {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.print("Enter a character: ");
+            char ch = sc.next().charAt(0);
+            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+                System.out.println("Alphabet");
+            else
+                System.out.println("Not an Alphabet");
+        }
+    }
+}
